308ae4019654c385bd6c7fd19bd155ea671465de
Utkarsh Solanki <160130824+UtkarshSolanki07@users.noreply.github.com>
2026-07-14 20:28:48 +0530

    fixing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import app from "./app";
-import { env } from "./config/env";
+import app from "./app.js";
+import { env } from "./config/env.js";
 
 app.listen(env.PORT, () => {
   console.log(`FindYourMatch API running on port ${env.PORT}`);
